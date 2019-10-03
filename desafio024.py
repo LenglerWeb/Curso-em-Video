bf7780e0089c414513b8c34c472a6464809a249a
Lengler Web <lenglerweb@gmail.com>
2019-10-03 16:50:55 -0300

    Novo desafio024.py

--- desafio024.py
+++ desafio024.py
@@ -1,4 +1,5 @@
 #  024: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
-cidade = str(input('Em que cidade vocÊ nasceu? '))
-sto = 'Santo' in cidade
-print(sto)
+cidade = str(input('Em que cidade vocÊ nasceu? ')).strip()
+l = cidade.lower()
+sto = 'santo' in l
+print('{}'.format(sto))

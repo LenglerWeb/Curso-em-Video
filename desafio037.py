67bee6c2882b6e7e548f10fed30ac57860b44651
Lengler Web <lenglerweb@gmail.com>
2019-10-11 17:24:01 -0300

    Adicionado o desafio037.py

--- desafio037.py
+++ desafio037.py
@@ -4,3 +4,21 @@
 # 2 para octal e 
 # 3 para hexadecimal.
 
+n = int(input('Digite um número inteiro: '))
+print('\033[1;32;41mEscolha uma base para conversão:\033[m')
+print('[ 1 ] converter para \033[7;30;42mBINÁRIO\033[m')
+print('[ 2 ] converter para \033[7;30;42mOCTAL\033[m')
+print('[ 3 ] converter para \033[7;30;42mHEXADECIMAL\033[m')
+n2 = int(input('Sua opção: '))
+
+if n2 == 1:
+   print('O número {}, convertido para BINÁRIO: {}'.format(n, bin(n)[2:]))
+
+elif n2 == 2:
+    print('O número {}, convertido para OCTAL: {}'.format(n, oct(n)[2:]))
+
+elif n2 == 3:
+    print('O número {}, convertido para HEXADECIMAL: {}'.format(n, hex(n)[2:]))
+
+else:
+    print('Opção INVÁLIDA!')

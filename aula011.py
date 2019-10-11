1ae3df6db7f2ac414ae396681adb87b7455cc8d9
Lengler Web <lenglerweb@gmail.com>
2019-10-11 11:32:50 -0300

    Adicionando aula011.py

---
+++ aula011.py
@@ -0,0 +1,11 @@
+# CORES NO TERMINAL
+
+#  \033[style;text;background m
+# exemplo:
+# \033[0;33;44m
+
+print('\033[1;31;43mEduardo Lengler\033[m')
+print('\033[1;30;45mEduardo Lengler\033[m')
+print('\033[1;36mEduardo Lengler\033[m')
+nome = 'Eduardo Lengler'
+print('Muito prazer em te conhecer, {}{}{}.'.format('\033[7;33m', nome, '\033[m'))

d6af64b4c97e64846e023f553de98e5917f3b73f
Lengler Web <lenglerweb@gmail.com>
2019-10-11 18:33:26 -0300

    Adicionando o desafio039.py

--- desafio038.py
+++ desafio038.py
@@ -3,3 +3,13 @@
 # - O primeiro valor é maior
 # - O segundo valor é maior
 # - Não existe valor maior, os dois são iguais.
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+if n1 > n2:
+    print('O \033[1;35mprimeiro\033[m número é MAIOR!')
+elif n2 > n1:
+    print('O \033[1;36msegundo\033[m número é MAIOR!')
+else:
+    print('Os \033[1;33mdois\033[m números são IGUAIS!')
---
+++ desafio039.py
@@ -0,0 +1,21 @@
+# 039: Faça um programa que leia o ano de nascimento de um jovem e informe, 
+# de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, 
+# se é a hora exata de se alistar ou se já passou do tempo do alistamento. 
+# Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+ano = int(input('Informe ano de nascimento: '))
+anoatual = 2019
+idade = anoatual - ano
+
+print('Quem nasceu em {} tem/terá {} anos em {}.'.format(ano, idade, anoatual))
+
+if idade < 18:
+    falta = 18 - idade
+    print('Ainda faltam {} anos para o alistamento.'.format(falta))
+    print('Seu alistamento será em {}.'.format(anoatual + falta))
+elif idade == 18:
+    print('Prepare-se para realizar o alistamento!')
+elif idade > 18:
+    passou = idade - 18
+    print('Você já deveria ter se alistado há {} anos.'.format(passou))
+    print('Seu alistamento foi em {}.'.format(anoatual - passou))

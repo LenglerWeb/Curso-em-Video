a3cdf21902ffad72ea3c44329c70183c82e12cce
Lengler Web <lenglerweb@gmail.com>
2019-10-17 19:15:14 -0300

    Atualizando o desafio041.py

--- desafio041.py
+++ desafio041.py
@@ -16,14 +16,16 @@ idade = anoatual - ano
 if idade > 25:
     print('O atleta tem {} anos e é \033[7;33;40mMASTER\033[m'.format(idade))
 
+# elif idade <= 25:
 elif idade >= 20 and idade <= 25:
     print('O atleta tem {} anos e é \033[7;33;40mSÊNIOR\033[m'.format(idade))
 
+# elif idade <= 19:
 elif idade >= 15 and idade < 20:
     print('O atleta tem {} anos e é \033[7;33;40mJÚNIOR\033[m'.format(idade))
-
+# elif idade <= 14:
 elif idade >= 10 and idade < 15:
     print('O atleta tem {} anos e é \033[7;33;40mINFANTIL\033[m'.format(idade))
-
+# elif idade <= 9:
 elif idade < 10:
     print('O atleta tem {} anos e é \033[7;33;40mMIRIM\033[m'.format(idade))

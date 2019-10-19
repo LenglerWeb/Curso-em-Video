113858c6301cd38efd9872295eeacf15f86f4b0a
Lengler Web <lenglerweb@gmail.com>
2019-10-18 21:26:34 -0300

    Nova atualização do desafio045.py

--- desafio045.py
+++ desafio045.py
@@ -1,7 +1,7 @@
 # Exercício Python 045: Crie um programa que faça o computador jogar Jokenpô com você.
 
 from random import randint
-
+itens = ('Pedra', 'Papel', 'Tesouras')
 print('-= JOKENPÔ =-')
 print('Escolha uma opção:')
 print('[ 0 ] PEDRA')
@@ -14,8 +14,8 @@ print('KEN')
 print('PÔ!!!')
 npc = randint(0,2)
 
-print('Computador jogou {}'.format(npc))
-print('Você escolheu {}'.format(escolha))
+print('Computador jogou {}'.format(itens[npc]))
+print('Você escolheu {}'.format(itens[escolha]))
 
 if escolha == 0 and npc == 2:
     print('Jogador Venceu!')
@@ -23,6 +23,13 @@ elif escolha == 1 and npc == 0:
     print('Jogador Venceu!')
 elif escolha == 2 and npc == 1:
     print('Jogador venceu')
+
+elif npc == 0 and escolha == 2:
+    print('Computador Venceu!')
+elif npc == 1 and escolha == 0:
+    print('Computador Venceu!')
+elif npc == 2 and escolha == 1:
+    print('Computador Venceu!')
 else:    
     print('\033[7;33;40mEMPATE!\033[m')
     

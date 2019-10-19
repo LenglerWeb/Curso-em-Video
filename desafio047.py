5806322b227c37480b35a943e7c235f07d037fac
Lengler Web <lenglerweb@gmail.com>
2019-10-19 16:51:21 -0300

    Atualizando o desafio047.py

--- desafio047.py
+++ desafio047.py
@@ -1,6 +1,7 @@
 # Exercício Python 047: Crie um programa que mostre na tela todos os números pares
 # que estão no intervalo entre 1 e 50.
 
+print('TODOS OS NUMEROS PARES ENTRE 1 E 50:')
 for c in range(2, 52, 2):
     print(c, end=' ')
 print('FIM')

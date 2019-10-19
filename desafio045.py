60f6504345556e9869baf186e7a411862180de8d
Lengler Web <lenglerweb@gmail.com>
2019-10-18 21:33:00 -0300

    2 Nova atualização do desafio045.py

--- desafio045.py
+++ desafio045.py
@@ -1,7 +1,8 @@
 # Exercício Python 045: Crie um programa que faça o computador jogar Jokenpô com você.
 
 from random import randint
-itens = ('Pedra', 'Papel', 'Tesouras')
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
 print('-= JOKENPÔ =-')
 print('Escolha uma opção:')
 print('[ 0 ] PEDRA')
@@ -10,26 +11,30 @@ print('[ 2 ] TESOURA')
 escolha = int(input('Qual a sua jogada? '))
 
 print('JO')
+sleep(1)
 print('KEN')
+sleep(1)
 print('PÔ!!!')
+sleep(1)
+
 npc = randint(0,2)
 
 print('Computador jogou {}'.format(itens[npc]))
 print('Você escolheu {}'.format(itens[escolha]))
 
 if escolha == 0 and npc == 2:
-    print('Jogador Venceu!')
+    print('\033[7;32;40mJogador Venceu!\033[m')
 elif escolha == 1 and npc == 0:
-    print('Jogador Venceu!')
+    print('\033[7;32;40mJogador Venceu!\033[m')
 elif escolha == 2 and npc == 1:
-    print('Jogador venceu')
+    print('\033[7;32;40mJogador venceu\033[m')
 
 elif npc == 0 and escolha == 2:
-    print('Computador Venceu!')
+    print('\033[7;31;40mComputador Venceu!\033[m')
 elif npc == 1 and escolha == 0:
-    print('Computador Venceu!')
+    print('\033[7;31;40mComputador Venceu!\033[m')
 elif npc == 2 and escolha == 1:
-    print('Computador Venceu!')
+    print('\033[7;31;40mComputador Venceu!\033[m')
 else:    
     print('\033[7;33;40mEMPATE!\033[m')
     

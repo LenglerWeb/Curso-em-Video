f46edc458bbb51c93d6dedee0b7af3567cebbc67
lenglerweb <lenglerweb@gmail.com>
2019-10-21 19:03:49 -0300

    Atualizando o desafio052.py

--- desafio052.py
+++ desafio052.py
@@ -5,10 +5,21 @@ num = int(input('Digite um número inteiro: '))
 total = 0
 for c in range(1, num+1):
     if num % c == 0:
+        # Se o número for PRIMO
         print('\033[1;36m', end='')
         total += 1
     else:
+        # Se o número NÃO for PRIMO
         print('\033[1;31m', end='')
+    
     print('{}'.format(c), end=' ')
+    
 print(' \033[m')
-print('O número {} foi disível {} vezes.'.format(num, total))
+print('=' * 30)
+print('\nO número {} foi disível {} vezes.'.format(num, total))
+
+if total == 2:
+    # Se o número for divisível APENAS 2 VEZES ele É PRIMO
+    print('O número {} é \033[7;32;40mPRIMO!!\033[m'.format(num))
+else:
+    print('O número {} \033[7;31;40mNÃO É PRIMO\033[m'.format(num))

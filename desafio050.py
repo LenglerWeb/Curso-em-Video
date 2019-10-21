d71cdf2c96aad64527726005df900dc0b69b65bc
Lengler Web <lenglerweb@gmail.com>
2019-10-20 21:00:18 -0300

    Adicionando o desafio050.py

--- desafio050.py
+++ desafio050.py
@@ -1,2 +1,20 @@
 # Exercício Python 050: Desenvolva um programa que leia seis números inteiros e mostre 
 # a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+
+print('Digite 6 números inteiros')
+print('=' * 15)
+s = 0
+impar = 0
+cont = 0
+for c in range(1, 7):
+    n = int(input('Digite o {} número: '.format(c)))
+    if (n % 2) == 0:
+        # s = n + s
+        s += n
+        # cont = cont + 1
+        cont += 1
+print('Foram {} número(s) par(es).'.format(cont))
+print('A soma dos números pares: {}'.format(s))    
+
+
+

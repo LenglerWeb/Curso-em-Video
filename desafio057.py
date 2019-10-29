f51b78d232c498d29eb88210cd26ac08b53e8ad5
lenglerweb <lenglerweb@gmail.com>
2019-10-28 21:28:47 -0300

    Atualizando o desafio057.pyfinalizado

--- desafio057.py
+++ desafio057.py
@@ -1,20 +1,30 @@
 # Exercício Python 057:
-# Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M'
-# ou 'F'.
-# Caso esteja errado, peça a digitação novamente até ter um valor correto.
+# Faça um programa que leia o sexo de uma pessoa, mas só
+# aceite os valores 'M' ou 'F'.
+# Caso esteja errado, peça a digitação novamente até ter
+# um valor correto.
 
-'''sair = 'S'
-sexo = ''
-print('Para SAIR do programa digite "S".')
-while sair == 'S':
-    sexo = str(input('Informe o seu sexo: [M/F]: ')).strip().upper()
-    sair = str(input('Deseja sair? [S/N]: ')).strip().upper()
-print('FIM')'''
-
-r = 'S'
+'''r = 'S'
 while r == 'S':
     n = int(input('Digite um número inteiro: '))
     r = str(input('Quer continuar? [S/N]')).strip().upper()
-print('FIM')
-               
+print('FIM')'''
+
+sair = ''
+sexo = ''
+print('Para SAIR do programa digite "S".')
+while sair != 'S':
+    sexo = str(input('Informe o seu sexo: [M/F]: ')).strip().upper()[0]
+    # while sexo != 'M' and sexo != 'F':
+    while sexo not in 'MmFf':
+        sexo = str(input('ERRO! Digite novamente o seu sexo: [M/F]: ')).strip().upper()[0]
+    print('Sexo {} registrado com SUCESSO.'.format(sexo))
     
+
+    sair = str(input('Deseja sair? [S/N]: ')).strip().upper()
+    while sair != 'S' and sair != 'N':
+        sair = str(input('ERRO! Digite somente [S/N]: ')).strip().upper()
+        
+      
+print('FIM')
+

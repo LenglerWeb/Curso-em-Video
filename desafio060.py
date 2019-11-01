e54e8c053a57dda3df23e750c9fbe5b38d9c320a
lenglerweb <lenglerweb@gmail.com>
2019-11-01 18:46:31 -0300

    Desafio60.py

--- desafio060.py
+++ desafio060.py
@@ -2,24 +2,53 @@
 
 # Ex: 5! = 5 x 4 x 3 x 2 x 1 = 120
 
+# Exemplo 1:
+from math import factorial
 print('=-' * 17)
 print('Calcular o FATORIAL de um número.')
 print('=-' * 17)
 
-f = 0
+
 n = int(input('Digite um número inteiro: '))
+f = factorial(n)
 
-while not n == 1:
-    f = n * (n - 1)
-    n -= 1
+print('O fatorial de {}! é {}'.format(n, f))
+print('FIM do Exemplo 1.')
+
+# Exemplo 2:
+print('\n')
+
+print('=-' * 17)
+print('Calcular o FATORIAL de um número.')
+print('=-' * 17)
+n = int(input('Digite um número inteiro: '))
+c = n
+f = 1
+print('Calculando {}! = '.format(n), end='')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
+print('\nFIM do Exemplo 2.')
+
+
+# Exemplo 3 com for:
+print('\n')
+
+print('=-' * 17)
+print('Calcular o FATORIAL de um número.')
+print('=-' * 17)
+n = int(input('Digite um número inteiro: '))
+c = n
+cont = 1
+for c in range(1, c+1):
     
-    #soma = f * n
-      
-   
+    print('')
+    f *= cont
+    print('{}'.format(f))
 
-    #print('o n = {}'.format(n))
-    print('o f = {}'.format(f))
-    #print('a SOMA = {}'.format(soma))
-    print('-' * 9)
-print('Total = {}'.format(f))
-print('FIM')
+
+
+print('\nFIM do Exemplo 3.')

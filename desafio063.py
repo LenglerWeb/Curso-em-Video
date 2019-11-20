9c9f9a18079f2f220ca814a7c0ee7c6c716bb0f1
lenglerweb <lenglerweb@gmail.com>
2019-11-20 18:50:09 -0300

    Atualizando o desafio063.py

--- desafio063.py
+++ desafio063.py
@@ -2,3 +2,19 @@
 # qualquer e mostre na tela os N primeiros elementos de uma Sequência de
 # Fibonacci. 
 
+print('-' * 23)
+print('Sequência de Fibonacci')
+print('-' * 23)
+
+n = int(input('Quantos termos você quer mostrar? '))
+termo = n
+cont = 1
+f = 0
+s = 0
+while cont <= termo:
+    print('{} -> '.format(s), end='')
+    f1 = 1
+    s = f + f1
+    
+    cont += 1
+print('FIM')

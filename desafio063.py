bf19d7d3f29d7ff2b90b10aed497b6a324be09ae
lenglerweb <lenglerweb@gmail.com>
2019-11-21 08:02:20 -0300

    Adicionando a minha versão do desafio063.py

--- desafio063.py
+++ desafio063.py
@@ -7,14 +7,19 @@ print('Sequência de Fibonacci')
 print('-' * 23)
 
 n = int(input('Quantos termos você quer mostrar? '))
-termo = n
-cont = 1
-f = 0
-s = 0
-while cont <= termo:
-    print('{} -> '.format(s), end='')
-    f1 = 1
-    s = f + f1
-    
+
+cont = 3
+f1 = 0
+f2 = 1
+f3 = 0
+
+print('~' * 30)
+print('{} -> {} ->'.format(f1, f2), end='')
+
+while cont <= n:
+    f3 = f1 + f2
+    print(' {} -> '.format(f3), end='')
+    f1 = f2
+    f2 = f3
     cont += 1
 print('FIM')
---
+++ desafio063_b.py
@@ -0,0 +1,24 @@
+# minha versão do programa do FIBONACCI - desafio063_b.py
+
+print('=~=' * 10)
+print('Sequência de FIBONACCI')
+print('=~=' * 10)
+
+n = int(input('Informe quantos termos deve mostrar: '))
+
+cont = 3
+f1 = 0
+f2 = 1
+f3 = 0
+
+print('{} -> {} '.format(f1, f2), end='')
+
+while cont <= n:
+    
+    f3 = f1 + f2
+    print('-> {} '.format(f3), end='')
+    f1 = f2
+    f2 = f3
+    cont += 1
+
+print('-> FIM')
